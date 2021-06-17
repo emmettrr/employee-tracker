7c1307331b6d7f48d8179f27c1d15874caa9baf9
Emmett Rushin <emmettrushin@gmail.com>
2021-06-16 23:53:16 -0400

    adjusted seed

--- seed/seed.sql
+++ seed/seed.sql
@@ -57,19 +57,19 @@ VALUE ("Lawyer", 190000, 4);
 
 
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Jessica", "Haze", null, 1);
+VALUE ("Burke", "Surf", null, 1);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tiffany", "Patric", null, 2);
+VALUE ("Emmett", "Rushin", null, 2);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Mia","Lam",null,3);
+VALUE ("Thomas","Spain",null,3);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Bently", "Lao", 1, 4);
+VALUE ("Luke", "Beard", 1, 4);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Chris", "Melby", 4, 5);
+VALUE ("Brittany", "Murphy", 4, 5);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Jason", "Baker", 1, 6);
+VALUE ("Jacob", "Anderson", 1, 6);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tom", "Nice", 2, 7);
+VALUE ("Tiffany", "Hi", 2, 7);
 
 
 SELECT * FROM department;
